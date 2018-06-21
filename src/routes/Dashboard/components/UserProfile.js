33bcbe497efa652847c410f5bc3b72dfdf291e72
Jorge Valdebenito Capdeville <jvaldebenito@smarti.cl>
2018-06-20 23:52:48 -0400

    add update password to user

--- src/routes/Dashboard/components/UserProfile.js
+++ src/routes/Dashboard/components/UserProfile.js
@@ -4,10 +4,14 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
 class UserProfile extends Component {
-    state = {
-        statePassword: false,
-        newPassword:'',
-        confirmPassword:'',
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            statePassword: false,
+            newPassword:'',
+            confirmPassword:'',
+        }
     }
 
     onClickPassword = () => {
@@ -25,13 +29,14 @@ class UserProfile extends Component {
         if( newPassword === confirmPassword
         && newPassword !== ''){
             return true;
-        }else{
+        } else{
             return false;
         }
     }
 
     onSubmit = () => {
         if(!this.itsOk()){
+            alert('Completar ambos campos');
             return console.log('error');
         }
 
@@ -44,20 +49,29 @@ class UserProfile extends Component {
 
         fetch('https://apifinder.herokuapp.com/user/change/',{
             method: 'PUT',
-            body: JSON.stringify(data),
             headers: {
-                'content-type': 'application/x-www-form-urlencoded'
-            }
+                "content-type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify(data),
         })
         .then(response => response.json())
         .then(resp => {
-            console.log(resp);
+            if(resp.status == 1){
+                alert('Password cambiado correctamente')
+            } else {
+                alert('Ups! ha ocurrido un problema, intenta más tarde')
+            }
+        })
+        .catch(error => {
+            alert('Ups! ha ocurrido un problema, intenta más tarde')
+            console.log(error)
         })
     }
 
     render(){
         return(
-            <div>
+            <div className="container-locations">
                 <h1> Perfil </h1>
                 <Grid container spacing={8} style={{width:"400px"}}>
                     <Grid item xs={12} sm={6} >
