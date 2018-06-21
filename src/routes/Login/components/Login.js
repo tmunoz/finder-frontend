e5e7de2e4f09bca2675bb319cba17a9b2e575a93
Jorge Valdebenito Capdeville <jvaldebenito@smarti.cl>
2018-06-21 00:22:22 -0400

    add create new user

--- src/routes/Login/components/Login.js
+++ src/routes/Login/components/Login.js
@@ -5,6 +5,7 @@ import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Modal from '@material-ui/core/Modal'
 
 import './Login.scss'
 
@@ -13,10 +14,16 @@ class Login extends Component{
   constructor (props) {
     super(props)
     this.state = {
+      name: '',
       username: '',
       password: '',
-      loadingLogin: false
+      loadingLogin: false,
+      createUser: false
     }
+  }
+
+  handleCreateUser = () => {
+    this.setState({ createUser: true })
   }
 
   handleChange = name => event => {
@@ -29,6 +36,25 @@ class Login extends Component{
     if (this.state.username === '' ||  this.state.password === '')
       return false
     return true
+  }
+
+  handleCreateUserOK = () => {
+    fetch('https://apifinder.herokuapp.com/user', {
+        method: 'POST',
+        headers: {
+          "content-type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          "name": this.state.name,
+          "email": this.state.username,
+          "password": this.state.password
+        }),
+      })
+      .then((response) => response.json())
+      .then(responseJSON => {
+        console.log(responseJSON.status)
+      })
   }
 
   handleLogin = () => {
@@ -65,6 +91,9 @@ class Login extends Component{
   render(){
     return(
       <div className="bg-login">
+
+      { this.state.createUser == false &&
+        
         <div className="content-login">
           { this.state.loadingLogin == true &&
             <div className="content-loading">
@@ -107,7 +136,70 @@ class Login extends Component{
               </Grid>
             </Grid>
           </form>
+          <br />
+          <button className="btn-create-user" onClick={this.handleCreateUser}>Crear usuario</button>
         </div>
+
+      }
+
+      { this.state.createUser == true &&
+        <div>
+          
+          <div className="content-login">
+            <Grid container spacing={0}>
+              <Grid item lg={12}>
+                <img src="logotipo-finder.png" width="100px" />
+                <h4>Crear Usuario</h4>
+              </Grid>
+            </Grid>
+            <form noValidate autoComplete="off">
+            <Grid container spacing={0}>
+                <Grid item lg={12}>
+                <TextField
+                    id="name"
+                    label="Nombre"
+                    value={this.state.name}
+                    onChange={this.handleChange('name')}
+                    margin="normal"
+                  />
+                </Grid>
+              </Grid>
+              <Grid container spacing={0}>
+                <Grid item lg={12}>
+                <TextField
+                    id="username"
+                    label="Email"
+                    value={this.state.username}
+                    onChange={this.handleChange('username')}
+                    margin="normal"
+                  />
+                </Grid>
+              </Grid>
+              <Grid container spacing={0}>
+                <Grid item lg={12}>
+                  <TextField
+                    id="password"
+                    label="Contraseña"
+                    type="password"
+                    onChange={this.handleChange('password')}
+                    margin="normal"
+                  />
+                </Grid>
+              </Grid>
+              <Grid container spacing={0}>
+                <Grid item lg={12}>
+                  <Button variant="raised" color="primary" onClick={this.handleCreateUserOK}>
+                    Crear usuario
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+
+        </div>
+      }
+
+        
       </div>
     );
   }
